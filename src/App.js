fcc8f9d0735a659e6b03d4ed5e7cf199126c7fc3
Serezha1993 <007serzh2014@mail.ru>
2025-03-23 19:10:48 +0300

    open menu

--- src/App.js
+++ src/App.js
@@ -7,75 +7,75 @@ import React, { useState } from 'react';
 const products = [
     {
       id: 1,
-      brand: 'samsung',
-      name: 'samsung s20',
+      brand: 'hp',
+      name: '155',
       price: 300,
-      category: 'phone',
+      category: 'monitor',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://images.philips.com/is/image/philipsconsumer/b9803f34eb74494582e9b014012a3fa6?wid=700&hei=700&$pnglarge$'
      },
      {
       id: 2,
       brand: 'samsung',
       name: 'samsung s20',
-      price: 300,
+      price: 900,
       category: 'phone',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://affordablephonesng.com/wp-content/uploads/2023/02/Samsung-Galaxy-S20-Plus.jpg'
      },
      {
       id: 3,
-      brand: 'samsung',
-      name: 'samsung s20',
-      price: 300,
+      brand: 'hp',
+      name: '155',
+      price: 550,
       category: 'monitor',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://images.philips.com/is/image/philipsconsumer/b9803f34eb74494582e9b014012a3fa6?wid=700&hei=700&$pnglarge$'
      },
      {
       id: 4,
-      brand: 'samsung',
-      name: 'samsung s20',
-      price: 300,
+      brand: 'hp',
+      name: '155',
+      price: 360, 
       category: 'monitor',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://images.philips.com/is/image/philipsconsumer/b9803f34eb74494582e9b014012a3fa6?wid=700&hei=700&$pnglarge$'
      },
      {
       id: 5,
-      brand: 'samsung',
-      name: 'samsung s20',
+      brand: 'apple',
+      name: 'iphone 13',
       price: 300,
       category: 'phone',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://www.largo.fr/4516-large_default/iphone-12-mini-128-go-blanc-reconditionne.jpg'
      },
      {
       id: 6,
       brand: 'samsung',
       name: 'samsung s20',
-      price: 300,
-      category: 'monitor',
+      price: 700,
+      category: '',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://affordablephonesng.com/wp-content/uploads/2023/02/Samsung-Galaxy-S20-Plus.jpg'
      },
      {
       id: 7,
-      brand: 'samsung',
-      name: 'samsung s20',
-      price: 300,
+      brand: 'macbook',
+      name: 'Air 15',
+      price: 200,
       category: 'laptop',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://img.5element.by/import/images/ut/goods/good_eb9dc94d-7028-11ef-8db4-005056012b6d/z1bq0009m-a3114-macbook-air-15-m3-8-10-8-512-space-gray-noutbuk-apple-1_600.jpg'
      },
      {
       id: 8,
-      brand: 'samsung',
-      name: 'samsung s20',
-      price: 300,
+      brand: 'macbook',
+      name: 'Air 15',
+      price: 800,
       category: 'laptop',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://img.5element.by/import/images/ut/goods/good_eb9dc94d-7028-11ef-8db4-005056012b6d/z1bq0009m-a3114-macbook-air-15-m3-8-10-8-512-space-gray-noutbuk-apple-1_600.jpg'
      },
      { 
       id: 9,
@@ -84,16 +84,16 @@ const products = [
       price: 300,
       category: 'phone',
       rating: 3,
-      img: 'https://m.media-amazon.com/images/I/71MHTD3uL4L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      img: 'https://www.largo.fr/4516-large_default/iphone-12-mini-128-go-blanc-reconditionne.jpg'
       },
       {
         id: 10,
-        brand: 'samsung',
-        name: 'samsung s20',
+        brand: 'macbook',
+        name: 'Air 15',
         price: 300,
         category: 'laptop',
         rating: 3,
-        img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+        img: 'https://img.5element.by/import/images/ut/goods/good_eb9dc94d-7028-11ef-8db4-005056012b6d/z1bq0009m-a3114-macbook-air-15-m3-8-10-8-512-space-gray-noutbuk-apple-1_600.jpg'
        },
        {
         id: 11,
@@ -102,36 +102,51 @@ const products = [
         price: 300,
         category: 'phone',
         rating: 3,
-        img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+        img: 'https://affordablephonesng.com/wp-content/uploads/2023/02/Samsung-Galaxy-S20-Plus.jpg'
        },
        {
         id: 12,
-        brand: 'samsung',
-        name: 'samsung s20',
-        price: 300,
+        brand: 'macbook',
+        name: 'Air 15',
+        price: 500,
         category: 'laptop',
-        rating: 3,
-        img: 'https://m.media-amazon.com/images/I/71l25hjYX1L.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+        rating: 5,
+        img: 'https://img.5element.by/import/images/ut/goods/good_eb9dc94d-7028-11ef-8db4-005056012b6d/z1bq0009m-a3114-macbook-air-15-m3-8-10-8-512-space-gray-noutbuk-apple-1_600.jpg'
        },
   ]
 
 function App () {
   const [inputName, setInputName] = useState('')
+  const [openNavbar, setOpenNavbar] = useState(false)
 
-  const filteredProduct = inputName ? products.filter((el) => el.brand === inputName) : products
+  const filteredProduct = inputName
+   ? products.filter((el) => el.brand.includes(inputName))
+   : products
 
   const handleInput = (text) => {
     setInputName(text)
   }
 
+  const handleOpen = () => {
+    setOpenNavbar(!openNavbar)
+  }
+
   return (
     <div>
-      <Header handleInput={handleInput}/>
+      <Header handleInput={handleInput} handleOpen={handleOpen}/>
+      {openNavbar && (
+      <div className="navbar">
+      <div>Телефоны</div>
+      <div className="active">Ноутбуки</div>
+      <div>Мониторы</div>
+      </div>
+      )}
      <div className="card-block">
       {filteredProduct.map((el) => (
         <Card 
         key={el.id}
         name={el.name}
+        brand={el.brand}
         img={el.img}
         rating={el.rating}
         price={el.price}
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,10 @@
-export const Card = ({ img, name, rating, price }) => {
+export const Card = ({ img, name, rating, price, brand }) => {
   return (
     <div>
       <div className="card">
         <img width={200} height={200} src={img} alt="здесь фото" />
         <div>{name}</div>
+        <h3>{brand}</h3>
         <div>Рейтинг: {rating}</div>
         <h3>${price}</h3>
       </div>
