89e4d8868f67cbcff866ff3840b4a223d5ea7ea6
Serezha1993 <007serzh2014@mail.ru>
2025-05-05 18:13:13 +0300

    totalPrice

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 import { ButtonBack } from "../../Components/buttonBack";
 import { CartItem } from "./CartItem";
-import "./index.css"
+import "./index.css";
 
 export const CartPage = (onClickFavorites) => {
   const { cart } = useSelector((state) => state.cart);
   const totalPrice = 4444;
+
+  const productCount = 124;
   return (
     <div>
       <div className="">
@@ -22,10 +24,14 @@ export const CartPage = (onClickFavorites) => {
         )}
       </div>
       <div className="totalPriceBlock">
-        <di>Количество</di>
-        <di>{productCount}</di>
-        <h3>Итого:</h3>
-        <h3 className="totalPrice">{totalPrice}</h3>
+        <div className="totalPriceRow">
+          <div>Количество шт.</div>&nbsp;&nbsp;
+          <b>{productCount}</b>
+        </div>
+        <div className="totalPriceRow">
+          <h3>Итого:</h3>
+          <h3 className="totalPrice">{totalPrice}</h3>
+        </div>
       </div>
       <ButtonBack />
     </div>
